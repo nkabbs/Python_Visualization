touched up color masking, added timestamp selection, and fixed region data visualization issue

json_loader.py
@@ -2,47 +2,40 @@ import json
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import random
 
 f = open('5_1_2.json', 'r')
 write_file = open('array_output.txt', 'a')
 json_data = json.loads(f.read())
 
-# def parse_signal_absorption_data(signal_absorption_data):
-#     parsed_data = []
-#     for i in range(len(signal_absorption_data)):
-#         density_data = signal_absorption_data[i]
-#         channels = density_data["channels"]
-#         guid = density_data["guid"]
-#         name = density_data["name"]
-#         region = density_data["region"]
-#         j = 0
-#         while j < len(channels):
-#             channel_data = channels[j]
-#             channel = channel_data["channel"]
-#             timestamp_data = channel_data["timestamp_data"]
-#             exploratory_signal_absorption = channel_data["exploratory_signal_absorption"]
-#             activating_signal_absorption = channel_data["activating_signal_absorption"]
-#             k = 0
-#             while k < len(timestamp_data):
-#                 parsed_data.append({"guid": guid, "name": name, "channel": channel, "timestamp": timestamp_data[k], "exploratory_signal_absorption": float(exploratory_signal_absorption[k]), "activating_signal_absorption": float(activating_signal_absorption[k])})
-#                 k += 1
-#             j += 1
-#     return parsed_data
-
 signal_absorption_data = json_data["signal_absorption_time_series_data"]
-#parsed_data = parse_signal_absorption_data(signal_absorption_data)
 
 df2 = pd.DataFrame(signal_absorption_data)
-
 regions = df2.region.unique()
 
-
-data_by_region = {}
 hide_zeroes = False
 exploratory = True
 
+example_timestamp_data = df2['channels'].values[0][0]['timestamp_data']
+
+selected_start_time = 90
+selected_end_time = 100
+timestamp_selection_start_index = 0
+timestamp_selection_end_index = 0
+#multiply by 2 because currently the timestamps are recorded at half-millisecond intervals (should somehow change that maybe?...)
+while timestamp_selection_start_index < len(example_timestamp_data) and example_timestamp_data[timestamp_selection_start_index] < selected_start_time and timestamp_selection_start_index < selected_end_time * 2:
+    timestamp_selection_start_index += 1
+while timestamp_selection_end_index < len(example_timestamp_data) and example_timestamp_data[timestamp_selection_end_index] < selected_end_time and timestamp_selection_end_index < selected_end_time * 2:
+    timestamp_selection_end_index += 1
+
 for region in regions:
+    if region == "Phoneme Density":
+        exploratory = False
+    fig, ax = plt.subplots()
     region_data = df2.loc[df2['region'] == region]
+
+    region_data_0 = region_data.values[0]
+
     data_by_density = []
     data_by_channel = []
     density_names = []
@@ -54,17 +47,22 @@ for region in regions:
         i += 1
     data_by_channel.reverse()
     density_names.reverse()
+
     for i in range(len(data_by_channel)):
         channel_signal_absorption_data = []
+        signal_absorption_index = timestamp_selection_start_index
         if exploratory:
-            for j in range(len(data_by_channel[i]['exploratory_signal_absorption'])):
-                channel_signal_absorption_data.append(data_by_channel[i]['exploratory_signal_absorption'][j])
+            while signal_absorption_index < timestamp_selection_end_index:
+            #for j in range(len(data_by_channel[i]['exploratory_signal_absorption'])):
+                channel_signal_absorption_data.append(data_by_channel[i]['exploratory_signal_absorption'][signal_absorption_index])
+                signal_absorption_index += 1
         else:
-            for j in range(len(data_by_channel[i]['activating_signal_absorption'])):
-                channel_signal_absorption_data.append(data_by_channel[i]['activating_signal_absorption'][j])
+            while signal_absorption_index < timestamp_selection_end_index:
+            #for j in range(len(data_by_channel[i]['activating_signal_absorption'])):
+                channel_signal_absorption_data.append(data_by_channel[i]['activating_signal_absorption'][signal_absorption_index])
+                signal_absorption_index += 1
         if not hide_zeroes or len([num for num in channel_signal_absorption_data if num > 0]) > 0:
             data_by_density.append(channel_signal_absorption_data)
-    data_by_region[region] = data_by_density
 
     plt.xlabel('time [ms]')
     plt.ylabel('Neuron')
@@ -85,9 +83,20 @@ for region in regions:
     plt.xticks(xticks, xlabels)
     plt.yticks(yticks, ylabels)
 
-    plt.title(("Exploratory " if exploratory else "Activating ") + "Signal Activation Mapping (" + region + ")")
+    plt.title(("Exploratory " if exploratory else "Activating ") + "Signal Activation Mapping ("
+              + str(region) + ": " + str(selected_start_time) + "ms to " + str(selected_end_time) + "ms)")
 
-    plt.imshow(data_by_density, cmap='Blues', aspect="auto", interpolation="nearest")
-    plt.savefig(region + '.png')
+    num_cols = len(data_by_density[0])
+    num_rows = len(data_by_density)
+    mask_values = [[1]*num_cols for i in range(num_rows)]
+    for i in range(num_rows):
+        for j in range(num_cols):
+            change_row_color = random.getrandbits(1)
+            if change_row_color:
+                mask_values[i][j] = 0
+    maskArr = np.ma.masked_array(data_by_density, mask=mask_values)
+    ax.imshow(data_by_density, cmap='Blues', aspect="auto", interpolation="nearest")
+    ax.imshow(maskArr, cmap='Reds', aspect="auto", interpolation="nearest")
+    #plt.savefig(region + '.png')
     plt.show()
 