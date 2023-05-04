a04adea1046933057bc0f0b3767a0139714d0bd9
Nick Abbs <nick@cambrya.co>
2023-05-04 14:55:35 -0500

    channel combination appears to be working, but only on test files so far

--- json_loader.py
+++ json_loader.py
@@ -10,17 +10,17 @@ json_data = json.loads(f.read())
 
 signal_absorption_data = json_data["signal_absorption_time_series_data"]
 
-df2 = pd.DataFrame(signal_absorption_data)
-regions = df2.region.unique()
+df = pd.DataFrame(signal_absorption_data)
+regions = df.region.unique()
 
 hide_zeroes = True
-exploratory = True
+exploratory = False
 combine_channels = True
 
-example_timestamp_data = df2['channels'].values[0][0]['timestamp_data']
+example_timestamp_data = df['channels'].values[0][0]['timestamp_data']
 
-selected_start_time = 0#20550 #20550 ~ when first clip starts
-selected_end_time = 33000#33000 #24000~ when first clip ends, 28500 ~ end of 3 samples
+selected_start_time = 0
+selected_end_time = 38000
 timestamp_selection_start_index = 0
 timestamp_selection_end_index = 0
 #multiply by 2 because currently the timestamps are recorded at half-millisecond intervals (should somehow change that maybe?...)
@@ -34,34 +34,63 @@ for region in regions:
         exploratory = False
         #region_data = region_data.sort_values(['layer'])
     fig, ax = plt.subplots()
-    region_data = df2.loc[df2['region'] == region]#.sort_values('name')
+    region_data = df.loc[df['region'] == region]#.sort_values('name')
     if region == "Pitch Transforms":
         region_data['name_length'] = region_data['name'].map(len)
         region_data = region_data.sort_values(['name_length', 'name'])
 
     data_by_density = []
-    data_by_channel = []
+    consolidated_signal_activation_data = []
     density_names = []
-    i = 0
-    num_channels = len(df2['channels'].values[0])
-    while i < num_channels:
-        data_by_channel.extend(o[i] for o in region_data['channels'].values)
-        density_names.extend(o + str(':') + str(i) for o in region_data['name'])
-        i += 1
-    data_by_channel.reverse()
+    num_channels = len(df['channels'].values[0])
+    if combine_channels:
+        for density_data in region_data['channels'].values:
+            highest_exploratory_signal_absorption = []
+            highest_activating_signal_absorption = []
+            highest_exploratory_signal_channel = []
+            highest_activating_signal_channel = []
+            for i in range(len(density_data[0]['timestamp_data'])):
+                timestamp_highest_exploratory_signal_absorption = 0
+                timestamp_highest_activating_signal_absorption = 0
+                j = 0
+                highest_exploratory_signal_channel.append(0)
+                highest_activating_signal_channel.append(0)
+                highest_exploratory_signal_absorption.append(0)
+                highest_activating_signal_absorption.append(0)
+                while j < num_channels:
+                    current_exploratory_signal_absorption = density_data[j]['exploratory_signal_absorption'][i]
+                    current_activating_signal_absorption = density_data[j]['activating_signal_absorption'][i]
+                    if current_exploratory_signal_absorption > timestamp_highest_exploratory_signal_absorption:
+                        highest_exploratory_signal_channel[i] = j
+                        highest_exploratory_signal_absorption[i] = current_exploratory_signal_absorption
+                        timestamp_highest_exploratory_signal_absorption = current_exploratory_signal_absorption
+                    if current_activating_signal_absorption > timestamp_highest_activating_signal_absorption:
+                        highest_activating_signal_channel[i] = j
+                        highest_activating_signal_absorption[i] = current_activating_signal_absorption
+                        timestamp_highest_activating_signal_absorption = current_activating_signal_absorption
+                    j += 1
+            consolidated_signal_activation_data.append({"timestamp_data": density_data[0]["timestamp_data"], "highest_exploratory_signal_channel": highest_exploratory_signal_channel, "highest_activating_signal_channel": highest_activating_signal_channel, "exploratory_signal_absorption": highest_exploratory_signal_absorption, "activating_signal_absorption": highest_activating_signal_absorption})
+            density_names.extend(o + str(':') + str(i) for o in region_data['name'])
+    else:
+        i = 0
+        while i < num_channels:
+            consolidated_signal_activation_data.extend(o[i] for o in region_data['channels'].values) #[{'primary_channel': [0,1], 'timestamp_data': [0, 0.5], 'exploratory_signal_absorption': [1, .1], 'activating_signal_absorption': [.1, .2]
+            density_names.extend(o + str(':') + str(i) for o in region_data['name'])
+            i += 1
+    consolidated_signal_activation_data.reverse()
     density_names.reverse()
 
     preserved_density_names = []
-    for i in range(len(data_by_channel)):
+    for i in range(len(consolidated_signal_activation_data)):
         channel_signal_absorption_data = []
         signal_absorption_index = timestamp_selection_start_index
         if exploratory:
             while signal_absorption_index < timestamp_selection_end_index:
-                channel_signal_absorption_data.append(data_by_channel[i]['exploratory_signal_absorption'][signal_absorption_index])
+                channel_signal_absorption_data.append(consolidated_signal_activation_data[i]['exploratory_signal_absorption'][signal_absorption_index])
                 signal_absorption_index += 1
         else:
             while signal_absorption_index < timestamp_selection_end_index:
-                channel_signal_absorption_data.append(data_by_channel[i]['activating_signal_absorption'][signal_absorption_index])
+                channel_signal_absorption_data.append(consolidated_signal_activation_data[i]['activating_signal_absorption'][signal_absorption_index])
                 signal_absorption_index += 1
         if not hide_zeroes or len([num for num in channel_signal_absorption_data if num > 0]) > 0:
             preserved_density_names.append(density_names[i])
@@ -71,7 +100,7 @@ for region in regions:
     plt.ylabel('Density')
     ylabels = []
     yticks = []
-    guids = np.unique(df2['guid'])
+    guids = np.unique(df['guid'])
     # for i in range(len(data_by_density)):
     for i in range(len(preserved_density_names)):
         ylabels.append(preserved_density_names[i])
@@ -97,11 +126,12 @@ for region in regions:
     num_cols = len(data_by_density[0])
     num_rows = len(data_by_density)
     mask_values = [[1]*num_cols for i in range(num_rows)]
-    for i in range(num_rows):
-        for j in range(num_cols):
-            change_row_color = random.getrandbits(1)
-            #if change_row_color:
-            #    mask_values[i][j] = 0
+    if combine_channels:
+        for i in range(num_rows):
+            current_density_data = consolidated_signal_activation_data[i]
+            for j in range(num_cols):
+                if (exploratory and current_density_data['highest_exploratory_signal_channel'][j] == 1) or (not exploratory and current_density_data['highest_activating_signal_channel'][j] == 1):
+                    mask_values[i][j] = 0
     maskArr = np.ma.masked_array(data_by_density, mask=mask_values)
     ax.imshow(data_by_density, cmap='Blues', aspect="auto", interpolation="nearest")
     ax.imshow(maskArr, cmap='Reds', aspect="auto", interpolation="nearest")
